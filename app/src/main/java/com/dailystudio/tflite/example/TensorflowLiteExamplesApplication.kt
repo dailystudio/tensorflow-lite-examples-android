4a5a28750d7441664138a9f21200860c5f3515d6
Nan YE <dailystudio2010@gmail.com>
2020-05-27 17:04:11 +0800

    [*][DEV]: project initialization;

---
+++ app/src/main/java/com/dailystudio/tflite/example/TensorflowLiteExamplesApplication.kt
@@ -0,0 +1,30 @@
+package com.dailystudio.tflite.example
+
+import com.dailystudio.devbricksx.app.DevBricksApplication
+import com.dailystudio.devbricksx.development.Logger
+import com.facebook.stetho.Stetho
+import com.nostra13.universalimageloader.core.ImageLoader
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
+
+class TensorflowLiteExamplesApplication : DevBricksApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.USE_STETHO) {
+            Stetho.initializeWithDefaults(this)
+        }
+
+        val config = ImageLoaderConfiguration.Builder(this).build()
+
+        ImageLoader.getInstance().init(config)
+
+        Logger.info("application is running in %s mode.",
+            if (BuildConfig.DEBUG) "DEBUG" else "RELEASE")
+    }
+
+    override fun isDebugBuild(): Boolean {
+        return BuildConfig.DEBUG
+    }
+
+}
