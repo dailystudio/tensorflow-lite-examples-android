15ed800191491280ce20cdd707d66814f9d11478
Nan YE <dailystudio2010@gmail.com>
2020-06-19 23:31:25 +0800

    [example-speech-recognition][INIT]: speech recognition initialization;

---
+++ example-speech-recognition/src/main/java/com/dailystudio/tflite/example/speech/recognition/ExampleActivity.kt
@@ -0,0 +1,12 @@
+package com.dailystudio.tflite.example.speech.recognition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
