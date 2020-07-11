fc7b55a772fac8131d71e60f7534b6d94f2ae548
Nan YE <dailystudio2010@gmail.com>
2020-07-11 23:57:12 +0800

    [buildscripts][DEV]: new script for download url task; [example-image-segmentation][INIT]: new example for image segmentation;

---
+++ example-image-segmentation/src/main/java/com/dailystudio/tflite/example/image/segmentation/ExampleActivity.kt
@@ -0,0 +1,25 @@
+package com.dailystudio.tflite.example.image.segmentation
+
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.dailystudio.tflite.example.common.AbsExampleActivity
+import com.dailystudio.tflite.example.common.image.ImageInferenceInfo
+
+class ExampleActivity : AbsExampleActivity<ImageInferenceInfo, Void>() {
+
+    override fun createBaseFragment(): Fragment {
+        return Fragment()
+    }
+
+    override fun createResultsView(): View? {
+        return null
+    }
+
+    override fun createSettingsView(): View? {
+        return null
+    }
+
+    override fun onResultsUpdated(results: Void) {
+    }
+
+}
