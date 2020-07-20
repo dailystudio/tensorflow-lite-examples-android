1332cbc42a198f4f87c9223345645cacf6b3cf8b
Nan YE <dailystudio2010@gmail.com>
2020-07-20 12:08:58 +0800

    [example-digit-classifier][INIT]: new module - digit classifier;

--- tensorflow-lite-examples-common/src/main/java/com/dailystudio/tflite/example/common/ui/InferenceInfoView.kt
+++ tensorflow-lite-examples-common/src/main/java/com/dailystudio/tflite/example/common/ui/InferenceInfoView.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.LinearLayout
@@ -51,6 +52,8 @@ class InferenceInfoView: FrameLayout {
 
     private fun setupViews() {
         itemsContainer = findViewById(R.id.items_container)
+
+        setInferenceInfo(InferenceInfo())
     }
 
     fun setColumns(cols: Int) {
