3a52c70b749d02d1da6ccdb9c934fdb860a28550
Nan YE <dailystudio2010@gmail.com>
2020-07-22 17:34:50 +0800

    [example-bert-qa][INIT]: new module - Bert Question&Answer;

---
+++ example-bert-qa/src/main/java/com/dailystudio/tflite/example/text/bertqa/ExampleActivity.kt
@@ -0,0 +1,25 @@
+package com.dailystudio.tflite.example.text.bertqa
+
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.dailystudio.tflite.example.common.AbsExampleActivity
+import com.dailystudio.tflite.example.common.InferenceInfo
+
+class ExampleActivity : AbsExampleActivity<InferenceInfo, Void>() {
+
+    override fun createBaseFragment(): Fragment {
+        return Fragment()
+    }
+
+    override fun createResultsView(): View? {
+        return null
+    }
+
+    override fun createSettingsView(): View? {
+        return null
+    }
+
+    override fun onResultsUpdated(results: Void) {
+    }
+
+}
