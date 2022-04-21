b509b94cb46a78d863d28f9cf657def9baf6fb93
Nan YE <dailystudio2010@gmail.com>
2022-04-21 16:08:27 +0800

    [example-speech-recognition][DEV]: adapt to latest api;

--- example-speech-recognition/src/main/java/com/dailystudio/tflite/example/speech/recognition/ExampleActivity.kt
+++ example-speech-recognition/src/main/java/com/dailystudio/tflite/example/speech/recognition/ExampleActivity.kt
@@ -1,25 +1,16 @@
 package com.dailystudio.tflite.example.speech.recognition
 
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.dailystudio.devbricksx.development.Logger
-import com.dailystudio.devbricksx.settings.AbsSettingsDialogFragment
-import com.dailystudio.devbricksx.utils.FileUtils
 import com.dailystudio.devbricksx.utils.StringUtils
 import com.dailystudio.tflite.example.common.AbsExampleActivity
-import com.dailystudio.tflite.example.common.InferenceInfo
 import com.dailystudio.tflite.example.speech.recognition.fragment.CommandsListFragment
 import com.dailystudio.tflite.example.speech.recognition.fragment.SpeechRecognitionFragment
 import com.dailystudio.tflite.example.speech.recognition.model.CommandViewModel
-import kotlinx.android.synthetic.main.activity_example_speech_recognition.*
-import kotlinx.android.synthetic.main.fragment_commands_list.*
 import org.tensorflow.lite.examples.speech.RecognizeCommands
-import org.tensorflow.lite.support.common.FileUtil
-import java.io.StringReader
 
 class ExampleActivity : AbsExampleActivity<AudioInferenceInfo, RecognizeCommands.RecognitionResult>() {
 
@@ -28,7 +19,10 @@ class ExampleActivity : AbsExampleActivity<AudioInferenceInfo, RecognizeCommands
 
         generateCommands()
 
-        (fragment_commands as CommandsListFragment).setRecyclerViewTouchEnabled(false)
+        val fragment = findFragment(R.id.fragment_commands)
+        if (fragment is CommandsListFragment) {
+            fragment.setRecyclerViewTouchEnabled(false)
+        }
     }
 
     private fun generateCommands() {
