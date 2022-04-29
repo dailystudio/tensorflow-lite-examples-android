c59a7b69693be3d29673d1a3b285225107887467
Nan YE <dailystudio2010@gmail.com>
2022-04-29 17:09:44 +0800

    [example-reinforcement-learning][NEW]: add new example for reinforcement learning; [app][DEV]: add support for new example

---
+++ example-reinforcement-learning/src/main/java/com/dailystudio/tflite/example/reinforcementlearning/ExampleActivity.kt
@@ -0,0 +1,37 @@
+package com.dailystudio.tflite.example.reinforcementlearning
+
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.dailystudio.tflite.example.common.AbsExampleActivity
+import com.dailystudio.tflite.example.common.InferenceInfo
+
+class ExampleActivity : AbsExampleActivity<InferenceInfo, Void>() {
+
+    override fun createBaseFragment(): Fragment {
+        return Fragment()
+    }
+
+    override fun createResultsView(): View? {
+        return null
+    }
+
+    override fun onResultsUpdated(results: Void) {
+    }
+
+    override fun onInferenceInfoUpdated(info: InferenceInfo) {
+        super.onInferenceInfoUpdated(info)
+    }
+
+    override fun getExampleName(): CharSequence? {
+        return getString(R.string.app_name)
+    }
+
+    override fun getExampleIconResource(): Int {
+        return R.drawable.about_icon
+    }
+
+    override fun getExampleDesc(): CharSequence? {
+        return getString(R.string.app_desc)
+    }
+
+}
