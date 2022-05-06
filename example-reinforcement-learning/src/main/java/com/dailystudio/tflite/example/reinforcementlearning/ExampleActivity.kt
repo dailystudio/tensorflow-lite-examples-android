55db04a9ce1e22de5432a2d4b4edd228d7165ec0
Nan YE <dailystudio2010@gmail.com>
2022-05-06 22:29:44 +0800

    [example-reinforcement-learning][DEV]: refine UI;

--- example-reinforcement-learning/src/main/java/com/dailystudio/tflite/example/reinforcementlearning/ExampleActivity.kt
+++ example-reinforcement-learning/src/main/java/com/dailystudio/tflite/example/reinforcementlearning/ExampleActivity.kt
@@ -1,17 +1,45 @@
 package com.dailystudio.tflite.example.reinforcementlearning
 
+import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.dailystudio.tflite.example.common.AbsExampleActivity
 import com.dailystudio.tflite.example.common.InferenceInfo
 import com.dailystudio.tflite.example.reinforcementlearning.fragment.ReinforcementLearningFragment
+import com.dailystudio.tflite.example.reinforcementlearning.viewmodel.AgentBoardViewModel
+import com.dailystudio.tflite.example.reinforcementlearning.viewmodel.PlayerBoardViewModel
 
 class ExampleActivity : AbsExampleActivity<InferenceInfo, Int>() {
 
     private var fabResetGame: View? = null
 
+    private lateinit var agentBoardViewModel: AgentBoardViewModel
+    private lateinit var playerBoardViewModel: PlayerBoardViewModel
+
+
+    private var agentHitsPrompt: TextView? = null
+    private var playerHitsPrompt: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        agentBoardViewModel = ViewModelProvider(this).get(AgentBoardViewModel::class.java)
+        agentBoardViewModel.hits.value.observe(this) {
+            playerHitsPrompt?.text = getString(R.string.player_hits_prompt, it)
+        }
+        playerBoardViewModel = ViewModelProvider(this).get(PlayerBoardViewModel::class.java)
+        playerBoardViewModel.hits.value.observe(this) {
+            agentHitsPrompt?.text = getString(R.string.agent_hits_prompt, it)
+        }
+    }
+
     override fun setupViews() {
         super.setupViews()
+
+        playerHitsPrompt = findViewById(R.id.player_hits)
+        agentHitsPrompt = findViewById(R.id.agent_hits)
 
         fabResetGame = findViewById(R.id.fab_reset)
         fabResetGame?.setOnClickListener {
