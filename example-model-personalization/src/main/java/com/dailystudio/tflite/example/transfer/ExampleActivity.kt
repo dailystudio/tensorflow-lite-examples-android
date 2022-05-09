4c59e1a3c295ee9a7b7e3adbbbba105eec2a7bb7
Nan YE <dailystudio2010@gmail.com>
2022-05-09 18:16:28 +0800

    [example-model-personalization][DEV]: update UI to support learning and inference;

--- example-model-personalization/src/main/java/com/dailystudio/tflite/example/transfer/ExampleActivity.kt
+++ example-model-personalization/src/main/java/com/dailystudio/tflite/example/transfer/ExampleActivity.kt
@@ -2,11 +2,42 @@ package com.dailystudio.tflite.example.transfer
 
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.dailystudio.devbricksx.development.Logger
 import com.dailystudio.tflite.example.common.AbsExampleActivity
 import com.dailystudio.tflite.example.common.InferenceInfo
 import com.dailystudio.tflite.example.transfer.fragment.TransferLearningCameraFragment
+import com.google.android.material.button.MaterialButton
 
 class ExampleActivity : AbsExampleActivity<InferenceInfo, Void>() {
+
+    companion object {
+        const val NUM_OF_CLASSES = 4
+
+    }
+    private var classButtons = arrayOfNulls<View?>(NUM_OF_CLASSES)
+
+    override fun setupViews() {
+        super.setupViews()
+
+        for (i in 0 until NUM_OF_CLASSES) {
+            val viewId = resources.getIdentifier("class_${i}_btn",
+                "id", packageName)
+            classButtons[i] = findViewById(viewId)
+            classButtons[i]?.setOnClickListener { view ->
+                val sampleClass = dumpSampleClassFromId(view.id)
+
+                Logger.debug("click on class: $sampleClass")
+
+                sampleClass?.let {
+                    (exampleFragment as? TransferLearningCameraFragment)?.addSamples(it)
+                }
+            }
+        }
+    }
+
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_example_model_personalization
+    }
 
     override fun createBaseFragment(): Fragment {
         return TransferLearningCameraFragment()
@@ -31,4 +62,16 @@ class ExampleActivity : AbsExampleActivity<InferenceInfo, Void>() {
         return getString(R.string.app_desc)
     }
 
+    private fun dumpSampleClassFromId(id: Int): String? {
+        for (i in 0 until NUM_OF_CLASSES) {
+            val viewId = resources.getIdentifier("class_${i}_btn",
+                "id", packageName)
+
+            if (id == viewId) {
+                return "${i + 1}"
+            }
+        }
+
+        return null
+    }
 }
--- example-model-personalization/src/main/java/com/dailystudio/tflite/example/transfer/fragment/TransferLearningCameraFragment.kt
+++ example-model-personalization/src/main/java/com/dailystudio/tflite/example/transfer/fragment/TransferLearningCameraFragment.kt
@@ -2,21 +2,33 @@ package com.dailystudio.tflite.example.transfer.fragment
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Matrix
+import com.dailystudio.devbricksx.development.Logger
 import com.dailystudio.tflite.example.common.image.AbsExampleCameraFragment
 import com.dailystudio.tflite.example.common.image.AbsImageAnalyzer
 import com.dailystudio.tflite.example.common.image.ImageInferenceInfo
 import com.dailystudio.tflite.example.common.ui.InferenceSettingsPrefs
 import com.dailystudio.tflite.example.transfer.model.TransferLearningModel
+import org.tensorflow.lite.examples.transfer.TransferLearningModelWrapper
 import org.tensorflow.lite.support.model.Model
 import org.tensorflow.litex.images.Recognition
+import java.util.*
+import kotlin.collections.ArrayDeque
 
 
-private class TransferLearningAnalyzer(rotation: Int,
-                                       lensFacing: Int,
-                                       useAverageTime: Boolean,
+private class TransferLearningAnalyzer(
+    rotation: Int,
+    lensFacing: Int,
+    useAverageTime: Boolean,
 ) : AbsImageAnalyzer<TransferLearningModel, ImageInferenceInfo, List<Recognition>>(rotation, lensFacing, useAverageTime) {
 
     companion object {
+    }
+
+    private val addSampleRequests = ArrayDeque<String>()
+
+    fun addSample(sampleClass: String) {
+        addSampleRequests.add(sampleClass)
     }
 
     override fun analyzeFrame(
@@ -26,11 +38,22 @@ private class TransferLearningAnalyzer(rotation: Int,
     ): List<Recognition>? {
         var results: List<Recognition>? = null
 
-        val start = System.currentTimeMillis()
-        results = model.analyze(inferenceBitmap)
-        val end = System.currentTimeMillis()
+        val sampleClass = addSampleRequests.removeFirstOrNull()
+        Logger.debug("[ANA TRACK]: sampleClass = $sampleClass")
+        if (sampleClass.isNullOrEmpty()) {
+            Logger.debug("[ANA TRACK]: do analysis")
+            // perform analysis
+            val start = System.currentTimeMillis()
+            results = model.analyze(inferenceBitmap)
+            val end = System.currentTimeMillis()
 
-        info.inferenceTime = (end - start)
+            info.inferenceTime = (end - start)
+        } else {
+            Logger.debug("[ANA TRACK]: add sample for class $sampleClass")
+            val data = TransferLearningModel.prepareCameraImage(
+                inferenceBitmap, info.screenRotation)
+            model.addSample(data, sampleClass)
+        }
 
         return results
     }
@@ -45,7 +68,9 @@ private class TransferLearningAnalyzer(rotation: Int,
         numOfThreads: Int,
         settings: InferenceSettingsPrefs
     ): TransferLearningModel? {
-        return TransferLearningModel(context, device, numOfThreads)
+        return TransferLearningModel(context, device, numOfThreads,
+            arrayListOf("1", "2", "3", "4")
+        )
     }
 
 }
@@ -59,6 +84,10 @@ class TransferLearningCameraFragment : AbsExampleCameraFragment<TransferLearning
         useAverageTime: Boolean,
     ): AbsImageAnalyzer<TransferLearningModel, ImageInferenceInfo, List<Recognition>> {
         return TransferLearningAnalyzer(rotation, lensFacing, useAverageTime)
+    }
+
+    fun addSamples(sampleClass: String) {
+        (analyzer as? TransferLearningAnalyzer)?.addSample(sampleClass)
     }
 
 }
